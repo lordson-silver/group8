60ab2eecaa44ed6cd8089ffbaa5db5f2eec61162
lordson-silver <lordsonochuko5@gmail.com>
2024-05-11 00:50:56 +0100

    Fix: Responsive for mobile device

--- components/nav.tsx
+++ components/nav.tsx
@@ -7,7 +7,7 @@ const Nav = ({
            className?:string;
    })  => {
   return (
-    <div className={cn(className, "flex justify-between items-center absolute top-0 m-auto px-10 pt-5 w-full")}>
+    <div className={cn(className, "flex justify-between items-center absolute top-0 m-auto px-2 mdpx-10 pt-5 w-full")}>
             {/* <h1 className='text-white text-xl flex-grow-0'>Logo</h1> */}
             <Image src={'/logo.png'} height={80} width={80} alt='Uniport Logo'/>
             <div className="flex flex-grow gap-4 justify-end">
