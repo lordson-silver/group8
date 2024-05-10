9be6f81d20d3dd9a6311e5b1d282d5ab053041bd
lordson-silver <lordsonochuko5@gmail.com>
2024-05-11 00:57:10 +0100

    Fix: Responsive Nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -7,10 +7,10 @@ const Nav = ({
            className?:string;
    })  => {
   return (
-    <div className={cn(className, "flex justify-between items-center absolute top-0 m-auto px-2 mdpx-10 pt-5 w-full")}>
+    <div className={cn(className, "flex justify-between items-center absolute top-0 m-auto px-2 md:px-10 pt-5 w-full")}>
             {/* <h1 className='text-white text-xl flex-grow-0'>Logo</h1> */}
-            <Image src={'/logo.png'} height={80} width={80} alt='Uniport Logo'/>
-            <div className="flex flex-grow gap-4 justify-end">
+            <Image src={'/logo.png'} className='-ml-5 md:m-0 object-cover' height={80} width={80} alt='Uniport Logo'/>
+            <div className="flex flex-grow gap-2 md:gap-4 justify-end">
                 <Button variant="outline" className='text-white' size="sm">Sign In</Button>
                 <Button size="sm">Sign Out</Button>
             </div>
