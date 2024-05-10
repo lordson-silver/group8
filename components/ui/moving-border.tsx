052a5724d66aab426eb59347c1e25972a9ce0126
lordson-silver <lordsonochuko5@gmail.com>
2024-05-11 00:39:24 +0100

    Fix: moving border

--- components/ui/moving-border.tsx
+++ components/ui/moving-border.tsx
@@ -47,7 +47,7 @@ export function Button({
         <MovingBorder duration={duration} rx="30%" ry="30%">
           <div
             className={cn(
-              " w-20 opacity-[0.9] bg-[radial-gradient(var(--white)_40%,transparent_60%)]",
+              "h-20 w-20 opacity-[0.9] bg-[radial-gradient(var(--white)_40%,transparent_60%)]",
               borderClassName
             )}
           />
