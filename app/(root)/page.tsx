a32f83806fcbf17fc715fdc4218ee89331b531f9
lordson-silver <lordsonochuko5@gmail.com>
2024-05-11 02:17:21 +0100

    Fix: Hero text gradient

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,13 +10,13 @@ export default function Home() {
     <div className="h-full w-full bg-black bg-grid-white  relative flex flex-col items-center justify-center overflow-hidden">
       <Nav className=""/>
       <div className="max-w-md md:max-w-[530px] w-full flex flex-col gap-5 items-center text-center justify-center">
-        <div className="flex items-center p-1 px-2 rounded-2xl border-2 border-gradient">
+        <div className="flex items-center p-1 px-2 rounded-2xl border-2 border-primary">
           <StarSVG className="mr-2" />
           <p className="text-sm text-white">Student Mentorship Matching Platform</p>
           <StarSVG className="ml-2" />
         </div>
 
-        <p className="text-4xl md:text-6xl bg-gradient text-white ">Empowering Students through Mentorship</p>
+        <h1 className="text-4xl md:text-6xl text-transparent bg-clip-text bg-gradient-to-r from-[#3A30DD] from-0% via-[#7C30DD] via-30% to-[#FE3876]">Empowering Students through Mentorship</h1>
         <p className="text-white text-sm">Connect with experienced mentors in your field, gain valuable insights, and explore exciting opportunities.</p>
           <Button
             borderRadius=".75rem"
